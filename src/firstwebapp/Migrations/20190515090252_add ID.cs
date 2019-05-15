ff3be359a89fd8ffc1da53f7d0ddbed7c975600a
Zeroks77 <dominik@mangatter.de>
2019-05-15 11:06:30 +0200

    add migration ^^

---
+++ src/firstwebapp/Migrations/20190515090252_add ID.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace firstwebapp.Migrations
+{
+    public partial class addID : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "EingereichtVon",
+                table: "Questions",
+                newName: "EingereichtVonID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "EingereichtVonID",
+                table: "Questions",
+                newName: "EingereichtVon");
+        }
+    }
+}
