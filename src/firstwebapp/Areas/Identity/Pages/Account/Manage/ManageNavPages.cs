7591db6b979644c5d8544006188ef1ed29a5e1fc
Zeroks77 <dominik@mangatter.de>
2019-05-17 09:45:36 +0200

    add privacy

--- src/firstwebapp/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
+++ src/firstwebapp/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
@@ -6,7 +6,8 @@ namespace firstwebapp.Areas.Identity.Pages.Account.Manage
     public static class ManageNavPages
     {
         public static string Index => "Index";
-
+        public static string GermanPrivacy => "GermanPrivacy";
+        public static string EnglishPrivacy => "EnglishPrivacy";
         public static string ChangePassword => "ChangePassword";
 
         public static string ExternalLogins => "ExternalLogins";
@@ -18,8 +19,9 @@ namespace firstwebapp.Areas.Identity.Pages.Account.Manage
         public static string TwoFactorAuthentication => "TwoFactorAuthentication";
 
         public static string IndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, Index);
-
-        public static string ShowUsersNavClass(ViewContext viewContext) => PageNavClass(viewContext, Index);
+        public static string GermanPrivacyNavClass(ViewContext viewContext) => PageNavClass(viewContext, GermanPrivacy);
+        public static string EnglishPrivacyNavClass(ViewContext viewContext) => PageNavClass(viewContext, EnglishPrivacy);
+        public static string ShowUsersNavClass(ViewContext viewContext) => PageNavClass(viewContext, ShowUsers);
 
         public static string ChangePasswordNavClass(ViewContext viewContext) => PageNavClass(viewContext, ChangePassword);
 
